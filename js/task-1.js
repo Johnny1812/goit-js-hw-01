9b066e54312b586f43033f06d513c7e87b2e7d8d
Johnny1812 <153526415+Johnny1812@users.noreply.github.com>
2024-02-24 11:29:14 +0200

    t1

---
+++ js/task-1.js
@@ -0,0 +1,13 @@
+function makeTransaction(quantity, pricePerDroid) {
+  const totalPrice = quantity * pricePerDroid;
+  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+}
+
+makeTransaction(5, 3, 10, 3000, 1000, 5000);
+
+// quantity(5, 3, 10);
+// pricePerDroid(3000, 1000, 500);
+
+console.log(makeTransaction(5, 3000));
+console.log(makeTransaction(3, 1000));
+console.log(makeTransaction(10, 500));
